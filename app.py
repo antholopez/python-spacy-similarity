1c2db2f209d38ddeef2bb9527bed0180225b1067
Anthony Lopez Vega <anthodev1195@gmail.com>
2022-08-16 00:44:03 -0500

    add spacy ok

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, jsonify, make_response
+import spacy;
+nlp = spacy.load('es_core_news_lg');
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_from_root():
+    doc1 = nlp('Cuales son los tipos de inteligencia artificial')
+    doc2 = nlp('que tipos de inteligencia artificial existe')
+    doc3 = nlp('que es la inteligencia artificial')
+    doc4 = nlp('la inteligencia artificial que tipos existen')
+    doc5 = nlp('¿Cuales son los tipos de inteligencia artificial?')
+
+    print(doc1.similarity(doc2)) 
+    print(doc1.similarity(doc3))
+    print(doc1.similarity(doc4))
+    print(doc1.similarity(doc5))
     return jsonify(message='Hello from root!')
 
 
