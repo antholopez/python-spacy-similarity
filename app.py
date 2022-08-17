0d9db5a4b5b7b544578818a1a4d6a6e97d16fc90
Anthony Lopez Vega <anthodev1195@gmail.com>
2022-08-16 22:51:32 -0500

    fix similarity ok

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
 import spacy;
 nlp = spacy.load('es_core_news_md');
 
@@ -7,24 +7,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_from_root():
-    print("Hello from root acaaaaaaaaaaaaaa")
-    doc1 = nlp('Cuales son los tipos de inteligencia artificial')
-    doc2 = nlp('que tipos de inteligencia artificial existe')
-    doc3 = nlp('que es la inteligencia artificial')
-    doc4 = nlp('la inteligencia artificial que tipos existen')
-    doc5 = nlp('¿Cuales son los tipos de inteligencia artificial?')
+    return jsonify(message="Hello from root")
 
-    print(doc1.similarity(doc2)) 
-    print(doc1.similarity(doc3))
-    print(doc1.similarity(doc4))
-    print(doc1.similarity(doc5))
-    return jsonify(message=doc1.similarity(doc2))
+@app.route("/similarity", methods=['POST'])
+def get_similarity():
+    text1 = request.json['text1']
+    text2 = request.json['text2']
+    doc1 = nlp(text1)
+    doc2 = nlp(text2)
+    similarity=round(doc1.similarity(doc2), 2)
 
-
-@app.route("/hello")
-def hello():
-    return jsonify(message='Hello from path!')
-
+    return jsonify({'similarity': similarity})
 
 @app.errorhandler(404)
 def resource_not_found(e):
